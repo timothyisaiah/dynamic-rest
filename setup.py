e00f813d1b3bff0511da28d84588830c2fa82689
aleontiev <alonetiev@gmail.com>
2018-08-09 15:40:01 -0700

    3.3.9: login html improvements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 NAME = 'dynamic-rest'
 DESCRIPTION = 'Adds Dynamic API support to Django REST Framework.'
 URL = 'http://github.com/AltSchool/dynamic-rest'
-VERSION = '3.3.7'
+VERSION = '3.3.9'
 SCRIPTS = ['manage.py']
 
 setup(
