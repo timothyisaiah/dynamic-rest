34400216929d235859dfa0e39ed0da4f926cda3d
aleontiev <alonetiev@gmail.com>
2018-08-14 10:38:05 -0700

    3.4.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 NAME = 'dynamic-rest'
 DESCRIPTION = 'Adds Dynamic API support to Django REST Framework.'
 URL = 'http://github.com/AltSchool/dynamic-rest'
-VERSION = '3.4.4'
+VERSION = '3.4.5'
 SCRIPTS = ['manage.py']
 
 setup(
