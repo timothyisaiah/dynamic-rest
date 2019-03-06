5bf1a0e0e28acc309ef44b31097108ef31eec757
aleontiev <alonetiev@gmail.com>
2019-03-05 18:05:09 -0800

    drest 3.6.2

--- dynamic_rest/static/dynamic_rest/js/drest-form.js
+++ dynamic_rest/static/dynamic_rest/js/drest-form.js
@@ -1,3 +1,74 @@
+function isMergeableObject(val) {
+    var nonNullObject = val && typeof val === 'object'
+
+    return nonNullObject
+        && Object.prototype.toString.call(val) !== '[object RegExp]'
+        && Object.prototype.toString.call(val) !== '[object Date]'
+}
+
+function emptyTarget(val) {
+    return Array.isArray(val) ? [] : {}
+}
+
+function cloneIfNecessary(value, optionsArgument) {
+    var clone = optionsArgument && optionsArgument.clone === true
+    return (clone && isMergeableObject(value)) ? deepmerge(emptyTarget(value), value, optionsArgument) : value
+}
+
+function defaultArrayMerge(target, source, optionsArgument) {
+    var destination = target.slice()
+    source.forEach(function(e, i) {
+        if (typeof destination[i] === 'undefined') {
+            destination[i] = cloneIfNecessary(e, optionsArgument)
+        } else if (isMergeableObject(e)) {
+            destination[i] = deepmerge(target[i], e, optionsArgument)
+        } else if (target.indexOf(e) === -1) {
+            destination.push(cloneIfNecessary(e, optionsArgument))
+        }
+    })
+    return destination
+}
+
+function mergeObject(target, source, optionsArgument) {
+    var destination = {}
+    if (isMergeableObject(target)) {
+        Object.keys(target).forEach(function (key) {
+            destination[key] = cloneIfNecessary(target[key], optionsArgument)
+        })
+    }
+    Object.keys(source).forEach(function (key) {
+        if (!isMergeableObject(source[key]) || !target[key]) {
+            destination[key] = cloneIfNecessary(source[key], optionsArgument)
+        } else {
+            destination[key] = deepmerge(target[key], source[key], optionsArgument)
+        }
+    })
+    return destination
+}
+
+function deepmerge(target, source, optionsArgument) {
+    var array = Array.isArray(source);
+    var options = optionsArgument || { arrayMerge: defaultArrayMerge }
+    var arrayMerge = options.arrayMerge || defaultArrayMerge
+
+    if (array) {
+        return Array.isArray(target) ? arrayMerge(target, source, optionsArgument) : cloneIfNecessary(source, optionsArgument)
+    } else {
+        return mergeObject(target, source, optionsArgument)
+    }
+}
+
+deepmerge.all = function deepmergeAll(array, optionsArgument) {
+    if (!Array.isArray(array) || array.length < 2) {
+        throw new Error('first argument should be an array with at least two elements')
+    }
+
+    // we are sure there are at least 2 values, so it is safe to have no initial value
+    return array.reduce(function(prev, next) {
+        return deepmerge(prev, next, optionsArgument)
+    })
+}
+
 function doGoto() {
     var goTo = $(this).data('goto');
     var target = $(this).data('goto-target') || '_self';
@@ -65,17 +136,6 @@ function throttle(func, wait, options) {
 
 /** dropify patches **/
 $(function(){
-
-    numeral.register('locale', 'drest', {
-        abbreviations: {
-            thousand: 'K',
-            million: 'M',
-            billion: 'B',
-            trillion: 'T'
-        }
-    });
-    numeral.locale('drest');
-
     Dropify.prototype.isTouchDevice = function() { return false; }
     Dropify.prototype.getFileType = function() {
         return this.file.name.split('.').pop().split('?').shift().toLowerCase();
@@ -1794,19 +1854,21 @@ function DRESTField(config) {
                 width: '100%',
                 height: 223,
                 toolbar: {
-                  download: false,
-                  selection: false,
-                  zoom: true,
-                  zoomin: true,
-                  zoomout: true,
-                  pan: true,
-                  reset: true
+                  tools: {
+                    download: false,
+                    selection: false,
+                    zoom: true,
+                    zoomin: true,
+                    zoomout: true,
+                    pan: false,
+                    reset: true
+                  },
+                  autoSelected: 'zoom'
                 },
-                autoSelected: 'zoom'
               }
             };
             if (value) {
-                this.value = value = Object.assign(value, defaults, value);
+                this.value = value = deepmerge.all([defaults, value]);
                 this.chart = new ApexCharts(
                     document.querySelector('#' + this.id + '-chart'),
                     value
